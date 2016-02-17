7159b11ded5d00fa15b69b27a84b9e434dfc3a34
Thomas <thomaslee3-c@my.cityu.edu.hk>
2016-02-17 19:37:01 +0800

    Resolve unused library

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -2,33 +2,6 @@
  * Created by Thomas on 6/8/15.
  */
 var Util = {};
-Util.promptMsg = function(message, position){
-    var n = noty({
-        text        : message,
-        type        : 'information',
-        dismissQueue: true,
-        layout      : position,
-        closeWith   : ['click'],
-        theme       : 'relax',
-        maxVisible  : 10,
-        animation   : {
-            open  : 'animated bounceInRight',
-            close : 'animated bounceOutRight',
-            easing: 'swing',
-            speed : 500
-        }
-    });
-
-    //setTimeout(function () {
-    //    $.noty.close(n.options.id);
-    //}, 5000);
-
-    Util.log('Prompted message id: ' + n.options.id);
-}
-
-Util.promptTopRightMsg = function(message){
-    Util.promptMsg(message, 'topRight');
-}
 
 Util.log = function(message){
     if(mode == 'debug')
